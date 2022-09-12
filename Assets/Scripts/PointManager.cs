d22182c7f702771999b1f0b67d4b29dd20540ddd
ryuoomiya <oomiyaryu@gmail.com>
2022-09-13 00:07:10 +0900

    アイテムを落としたときに入れ替えるコードを追加

--- Assets/Scripts/PointManager.cs
+++ Assets/Scripts/PointManager.cs
@@ -4,22 +4,44 @@ using UnityEngine;
 
 public class PointManager : MonoBehaviour
 {
+    [SerializeField, Tooltip("拾えるアイテム")]
+    List<GameObject> _hitItems = new List<GameObject>();
     GameObject _haveObject;
     ItemGrid _gridScript;
     IPickableItem _iPickScript;
-    [Tooltip("ツール持ってる?")]bool _isHave = false;
+    [Tooltip("ツール持ってる?")] bool _isHave = false;
 
-    private void Start()
+    void Start()
     {
         _gridScript = GetComponent<ItemGrid>();
     }
-    private void Update()
+    void Update()
     {
-        ItemDrop();
+        //アイテム拾うか落とすか
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (_isHave)
+            {
+                ItemDrop();
+            }
+            if (_hitItems.Count > 0)
+            {
+                ToolSeach(_hitItems[0]);
+            }
+        }
     }
-    private void OnTriggerStay(Collider other)
+
+    private void OnTriggerEnter(Collider other)
     {
-        ToolSeach(other.gameObject);
+        if (other.gameObject.GetComponent<IPickableItem>() != null)
+        {
+            _hitItems.Add(other.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _hitItems.Remove(other.gameObject);
     }
 
     /// <summary>
@@ -28,13 +50,11 @@ public class PointManager : MonoBehaviour
     /// <param name="hitObj">当たったobj</param>
     public void ToolSeach(GameObject hitObj)
     {
+        Debug.Log("入ってます");
         if (hitObj.TryGetComponent(out IPickableItem items))
         {
-            //Debug.Log("入ってます");
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                ItemPick(hitObj ,items);
-            }
+
+            ItemPick(hitObj, items);
         }
 
         if (_iPickScript != null)
@@ -42,12 +62,13 @@ public class PointManager : MonoBehaviour
             _iPickScript.Action(hitObj);
         }
     }
-    
+
     /// <summary>
     /// アイテム取る
     /// </summary>
-    void ItemPick(GameObject hitObj , IPickableItem items)
+    void ItemPick(GameObject hitObj, IPickableItem items)
     {
+        _hitItems.Remove(hitObj);
         _iPickScript = items;
         _haveObject = hitObj;
         _isHave = true;
@@ -55,25 +76,19 @@ public class PointManager : MonoBehaviour
         hitObj.transform.parent = this.gameObject.transform;
         Debug.Log("Toolを取った");
     }
-    
+
     /// <summary>
     /// アイテム落とす
     /// </summary>
     private void ItemDrop()
     {
-        if (_isHave)
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                Debug.Log("アイテムを落とした");
-                _haveObject.transform.position =
-                   new Vector3(_gridScript.Point.transform.position.x, 
-                                0, _gridScript.Point.transform.position.z); //一番近いマスに落とす
-                _haveObject.transform.parent = null;
-                _haveObject = null;     //落としたらリセット
-                _iPickScript = null;    //落としたらリセット
-                _isHave = false;
-            }
-        }
+        _isHave = false;
+        Debug.Log("アイテムを落とした");
+        _haveObject.transform.position =
+           new Vector3(_gridScript.Point.transform.position.x,
+                        0, _gridScript.Point.transform.position.z); //一番近いマスに落とす
+        _haveObject.transform.parent = null;
+        _haveObject = null;     //落としたらリセット
+        _iPickScript = null;    //落としたらリセット
     }
 }
