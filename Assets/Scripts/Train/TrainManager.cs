16c9708192d8c275758984dc2eeeb81833fbaabf
ryuoomiya <oomiyaryu@gmail.com>
2022-09-27 01:10:35 +0900

    アクシデントでデータが飛ぶ危険があるのでエラー吐いてるけど追加

--- Assets/Scripts/Train/TrainManager.cs
+++ Assets/Scripts/Train/TrainManager.cs
@@ -4,20 +4,56 @@ using UnityEngine;
 
 public class TrainManager : MonoBehaviour
 {
-    [Tooltip("列車の進むスピード")]float _moveSpeed = 10.0f;
-    [Tooltip("列車のRIgidbody")]Rigidbody _rb;
+    [SerializeField, Tooltip("列車の進むスピード")] float _moveSpeed = 0.02f;
+    [Tooltip("列車のRIgidbody")] Rigidbody _rb;
+    [SerializeField, Tooltip("左Raycast")] Transform _leftR;
+    [SerializeField, Tooltip("右Raycast")] Transform _rightR;
+    [SerializeField, Tooltip("前Raycast")] Transform _frontR;
+
+    bool _isRotate;
     void Start()
     {
-        _rb = GetComponent<Rigidbody>(); 
+        _rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        Move();
+        TrainRotate();
     }
 
-    void Move()
+    /// <summary>Trainの進行用にRayを飛ばす</summary>
+    void TrainRaycast()
     {
-        _rb.AddForce(transform.right * _moveSpeed);
+        if(Raycast(_frontR))
+        {
+
+        }
+    }
+
+    /// <summary>Rayを飛ばす</summary>
+    /// <param name="rayPos">Rayを飛ばす向き</param>
+    Vector3 Raycast(Transform rayPos)
+    {
+        Vector3 trainPos = transform.position;
+
+        Vector3 rayVec = rayPos.position - transform.position;
+        RaycastHit hit;
+        if (Physics.Raycast(trainPos, rayVec, out hit, 3))
+        {
+            if(hit.collider.TryGetComponent(out Rail rail) && hit.collider == RailManager.Instance._rails[RailManager.Instance._rails.Count - 1])
+            {
+                return hit.collider.transform.position;
+              
+            }
+        }
+        Debug.DrawRay(trainPos, rayVec);
+    }
+    void TrainRotate()
+    {
+
+    }
+    void TrainMove()
+    {
+        _rb.AddForce(transform.right * _moveSpeed, ForceMode.Force);
     }
 }
