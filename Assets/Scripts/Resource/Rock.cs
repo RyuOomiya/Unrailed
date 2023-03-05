ffc9b2c5928de4a7dc2c11b43147bcd7f1854730
ryuoomiya <oomiyaryu@gmail.com>
2023-03-05 14:17:40 +0900

    [Update] TreeとRockのライフが削れると小さくなっていく機能を実装

--- Assets/Scripts/Resource/Rock.cs
+++ Assets/Scripts/Resource/Rock.cs
@@ -7,14 +7,18 @@ public class Rock : MonoBehaviour, IPickableItem
     [Tooltip("アイテムタイプ"), SerializeField] ItemType _type;
     int _rockLife = 3;
     [SerializeField] GameObject _stone;
+    [SerializeField] GameObject _renderer;
     public int RockLife { get { return _rockLife; } set { _rockLife = value; } }
-    
+
     void Update()
     {
+        if (_rockLife == 3) _renderer.transform.localScale = new Vector3(3, 3, 3);
+        if (_rockLife == 2) _renderer.transform.localScale = new Vector3(2, 2, 2);
+        if (_rockLife == 1) _renderer.transform.localScale = new Vector3(1, 1, 1);
         if (_rockLife <= 0)
         {
             PointManager.Instance._hitItems.Remove(gameObject);
-            Instantiate(_stone , gameObject.transform.position , Quaternion.identity);
+            Instantiate(_stone, gameObject.transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
     }
--- Assets/Scripts/Resource/Tree.cs
+++ Assets/Scripts/Resource/Tree.cs
@@ -7,11 +7,15 @@ public class Tree : MonoBehaviour , IPickableItem
     [Tooltip("アイテムタイプ"), SerializeField] ItemType _type;
     int _treeLife = 3;
     public int TreeLife { get { return _treeLife; } set { _treeLife = value; } }
-    [SerializeField]GameObject _wood;
+    [SerializeField] GameObject _wood;
+    [SerializeField] GameObject _renderer;
    
     void Update()
     {
-        if(_treeLife <= 0)
+        if (_treeLife == 3) _renderer.transform.localScale = new Vector3(3, 3, 3);
+        if (_treeLife == 2) _renderer.transform.localScale = new Vector3(2, 2, 2);
+        if (_treeLife == 1) _renderer.transform.localScale = new Vector3(1, 1, 1);
+        if (_treeLife <= 0)
         {
             
             PointManager.Instance._hitItems.Remove(gameObject);
